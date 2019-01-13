f94ad5e641abdf9941c0ddd79e5ea21b8294ba6e
Kostyk163 <46649456+Kostyk163@users.noreply.github.com>
2019-01-13 18:15:56 +0400

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,7 @@
+<?php
+$menu = [
+    ['link' => 'Главная', 'href' => 'index.html'],
+    ['link' => 'Календарь', 'href' => 'calendar.php'],
+    ['link' => 'Создать событие', 'href' => 'new_event.php'],
+    ['link' => 'Мои события', 'href' => 'event.php'],
+];
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+include 'menu.php';
+?>
+<ul>
+    <li><a href='<?php include 'menu.php'; echo $menu[0]['href']?>'><?php include 'menu.php'; echo $menu[0]['link']?></a></li>
+    <li><a href='<?php include 'menu.php'; echo $menu[1]['href']?>'><?php include 'menu.php'; echo $menu[1]['link']?></a></li>
+    <li><a href='<?php include 'menu.php'; echo $menu[2]['href']?>'><?php include 'menu.php'; echo $menu[2]['link']?></a></li>
+    <li><a href='<?php include 'menu.php'; echo $menu[3]['href']?>'><?php include 'menu.php'; echo $menu[3]['link']?></a></li>
+</ul>
