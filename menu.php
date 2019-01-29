89fe282059e797a1af2ab4fa9c84c5ab02be91c2
Kostyk163 <46649456+Kostyk163@users.noreply.github.com>
2019-01-29 20:50:34 +0300

    Delete menu.php

--- menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-$menu = [
-    ['link' => 'Главная', 'href' => 'index.html'],
-    ['link' => 'Календарь', 'href' => 'calendar.php'],
-    ['link' => 'Создать событие', 'href' => 'new_event.php'],
-    ['link' => 'Мои события', 'href' => 'event.php'],
-];
